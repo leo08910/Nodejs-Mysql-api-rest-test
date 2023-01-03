8c57c1401cfaba0742c0826cf11dd820b5576308
leo08910 <leo_torres089@outlook.com>
2023-01-03 03:40:47 -0300

    first commit

---
+++ src/controllers/employees.controller.js
@@ -0,0 +1,99 @@
+import { json } from "express";
+import { pool } from "../db.js";
+
+export const getEmployees = async (req, res) => {
+  try {
+    const [rows] = await pool.query("SELECT * FROM employee");
+    res.json(rows);
+  } catch (error) {
+    return res.status(500).json({
+      message: "Error",
+    });
+  }
+};
+
+export const getEmployee = async (req, res) => {
+  try {
+    const [rows] = await pool.query("SELECT * FROM employee WHERE id = ?", [
+      req.params.id,
+    ]);
+
+    if (rows.length <= 0)
+      return res.status(404).json({
+        message: "Employee not found",
+      });
+
+    console.log(rows[0]);
+    res.json(rows[0]);
+  } catch (error) {
+    return res.status(500).json({
+      message: "Error",
+    });
+  }
+};
+
+export const createEmployees = async (req, res) => {
+  try {
+    const { name, salary } = req.body;
+    const [rows] = await pool.query(
+      "INSERT INTO employee(name, salary) VALUES (?,?)",
+      [name, salary]
+    );
+    res.send({
+      id: rows.insertId,
+      name,
+      salary,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Error",
+    });
+  }
+};
+
+export const deleteEmployees = async (req, res) => {
+  try {
+    const [result] = await pool.query("DELETE FROM employee WHERE id = ?", [
+      req.params.id,
+    ]);
+
+    if (result.affectedRows <= 0)
+      return res.status(404).json({
+        message: "Employee not found",
+      });
+
+    res.sendStatus(204);
+  } catch (error) {
+    return res.status(500).json({
+      message: "Error",
+    });
+  }
+};
+
+export const updateEmployees = async (req, res) => {
+  const { id } = req.params; // Equivale a const id = req.params.id
+  const { name, salary } = req.body; // Equivale a const name = req.body.name, etc
+
+  try {
+    const [result] = await pool.query(
+      "UPDATE employee SET name = IFNULL(?, name), salary = IFNULL(?, salary) WHERE id = ?",
+      [name, salary, id]
+    );
+    console.log(result);
+
+    if (result.affectedRows === 0)
+      return res.status(404).json({
+        message: "Employee not found",
+      });
+
+    const [rows] = await pool.query("SELECT * FROM employee WHERE id = ?", [
+      id,
+    ]);
+
+    res.json(rows[0]);
+  } catch (error) {
+    return res.send(500).json({
+      message: "Error",
+    });
+  }
+};
